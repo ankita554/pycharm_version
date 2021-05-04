f6a19e057e72253765821e7dd35b1598137c590c
ankita554 <ankitajaiswal93@gmail.com>
2021-05-05 00:27:00 +0530

    ok

---
+++ main.py
@@ -0,0 +1,15 @@
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+my_list = sorted(some_list)
+
+duplicates = []
+for i in my_list:
+    if my_list.count(i) > 1:
+
+     if i not in duplicates:
+
+         duplicates.append(i)
+
+print(len(duplicates))
+print(duplicates)
+print("name")
